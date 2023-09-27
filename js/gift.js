2152d1ccb45c33ec29df6bc55c9a69234d064f5a
ohyuuurim <ohyr12@hanmail.net>
2023-09-27 18:55:25 +0900

    2023.09.27 오유림

---
+++ js/gift.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  /* 푸터 아코디언 */
+  $(".fTitle").click(function(){
+
+    $(this).siblings(".fTitle").removeClass("active");
+    $(this).toggleClass("active");
+    $(this).siblings(".fContent").stop().slideUp();
+    $(this).next().stop().slideToggle();
+  });
+
+  /* Like Button */
+  $('.product img:last-child').click(function(){
+    let src = ($(this).attr('src') === 'image/heart.svg')
+      ? 'image/heart-fill.png'
+      : 'image/heart.svg';
+    $(this).attr('src', src);
+});
+
+});
